73c74b813ac796a9e9699485dffd07dabc7c5c14
Kiyoon Kim <yoonkr33@gmail.com>
2023-01-20 18:46:30 +0000

    jupynium

--- lua/user/jupynium.lua
+++ lua/user/jupynium.lua
@@ -5,6 +5,7 @@ end
 
 jupynium.setup {
   python_host = "~/bin/miniconda3/envs/jupynium/bin/python",
+  jupyter_command = "~/bin/miniconda3/envs/jupynium/bin/jupyter",
   -- auto_attach_server = { enable = false },
   -- auto_start_server = { enable = true },
   -- auto_start_sync = { enable = true },
