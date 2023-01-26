16697ce2d7bc8793acbfc22feef8c8f19173662c
Kiyoon Kim <yoonkr33@gmail.com>
2023-01-26 16:54:11 +0000

    .

--- lua/user/jupynium.lua
+++ lua/user/jupynium.lua
@@ -5,8 +5,29 @@ end
 
 jupynium.setup {
   python_host = "~/bin/miniconda3/envs/jupynium/bin/python",
-  jupyter_command = "~/bin/miniconda3/envs/jupynium/bin/jupyter",
-  -- auto_attach_server = { enable = false },
-  -- auto_start_server = { enable = true },
-  -- auto_start_sync = { enable = true },
+  jupyter_command = "~/bin/miniconda3/bin/jupyter",
+
+  -- Open the Jupynium server if it is not already running
+  -- which means that it will open the Selenium browser when you open this file.
+  -- Related command :JupyniumStartAndAttachToServer
+  auto_start_server = {
+    enable = false,
+    file_pattern = { "*.ju.*" },
+  },
+
+  -- Attach current nvim to the Jupynium server
+  -- Without this step, you can't use :JupyniumStartSync
+  -- Related command :JupyniumAttachToServer
+  auto_attach_to_server = {
+    enable = true,
+    file_pattern = { "*.ju.*" },
+  },
+
+  -- Automatically open an Untitled.ipynb file on Notebook
+  -- when you open a .ju.py file on nvim.
+  -- Related command :JupyniumStartSync
+  auto_start_sync = {
+    enable = false,
+    file_pattern = { "*.ju.*" },
+  },
 }
